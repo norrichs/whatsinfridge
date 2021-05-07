070e8bf069697578991be7bf97f733daf0d02b5f
Ben Norrichs <ben.norrichs@gmail.com>
2021-05-07 12:41:21 -0400

    cleaned up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
 		<header>
 			<div><FontAwesomeIcon icon={faUtensils}/></div>
 			<nav>
-				<NavLink to='/'><div>new search</div></NavLink>
-				<NavLink to='/Results'><div>refine results</div></NavLink>
+				<NavLink to='/'><div>search</div></NavLink>
+				<NavLink to='/Results'><div>results</div></NavLink>
 				<NavLink to='/Recipes'><div>recipes</div></NavLink>
-				<NavLink to='/List'><div>shopping list</div></NavLink>
+				<NavLink to='/List'><div>shopping</div></NavLink>
 				<NavLink to='/About'><div>about</div></NavLink>
 			</nav>
 		</header>
